4be7de9d059760e074bbd238417dee0d0f146e00
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-06-13 16:35:38 +0100

    initial commit

---
+++ bear_fish_river.rb
@@ -0,0 +1,19 @@
+class Bear
+  def initialize()
+    @roar
+    @type
+    @name
+    @tummy
+  end
+end
+
+class Fish
+  def initialize()
+    @name
+  end
+end
+
+class River
+  def initialize()
+  end
+end
---
+++ specs/bear_fish_river_specs.rb
@@ -0,0 +1,12 @@
+require ('minitest/autorun')
+require_relative('../bear_fish_river')
+
+class TestBear < MiniTest::Test
+end
+
+class TestFish < MiniTest::Test
+end
+
+class TestRiver < MiniTest::Test
+end
+
